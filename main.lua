476425d341d1bb1bfabee544da37681fe00472e0
brennop <brennopcordeiro@gmail.com>
2023-06-18 17:20:51 -0300

    add world and body

--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
 Object = require "lib.classic"
 
+local _world, _car
+
 function love.load()
+  _world = love.physics.newWorld(0, 0, true)
+
+  local Car = require "car"
+  _car = Car(_world, 100, 100)
 end
 
 function love.update(dt)
+  _car:update(dt)
+  _world:update(dt)
 end
 
 function love.draw()
+  _car:draw()
 end
--- objects/car.lua
+++ objects/car.lua
@@ -4,8 +4,10 @@ local Car = Object:extend()
 
 function Car:new(world, x, y)
   self.position = Vector(x, y)
-
   self.speed = 100
+
+  self.world = world
+  self.body = love.physics.newBody(self.world, self.position.x, self.position.y, 'dynamic')
 end
 
 function Car:update(dt)
